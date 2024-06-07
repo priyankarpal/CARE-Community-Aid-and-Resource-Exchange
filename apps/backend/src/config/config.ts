26f42a0ff974e06c4190d14e2bb40942ea0e37cb
Puskar Roy <113108193+Puskar-Roy@users.noreply.github.com>
2024-06-07 09:48:22 +0530

    feat : limiter and cors (#6)
    * feat : add uuid
    
    * feat : error handeler
    
    * feat : limiter and cors

---
+++ apps/backend/src/config/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+  PORT: process.env.PORT || 5050,
+  DEV_ENV : process.env.DEV_ENV || "DEV"
+};
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,13 +1,29 @@
-import express from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
+import { config } from "./config/config";
+import corsOptions from "./lib/corsConfig";
+import limiter from "./lib/rateLimitConfig";
+import CheckError from "./lib/checkError";
+import errorHandler from "./middleware/errorMiddleware";
+import cors from "cors";
+import morgan from "morgan";
 
-const app = express();
+const app: Express = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(config.DEV_ENV === "PROD" ? cors(corsOptions) : cors());
+app.use(limiter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ success: true });
 });
 
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  next(new CheckError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
-app.listen(5050,()=>{
-    console.log("Server Is Running on PORT 5050");
-    
-})
+app.use(errorHandler);
+
+app.listen(config.PORT, () => {
+  console.log(`[⚡] Server Is Running on http://localhost:${config.PORT}`);
+});
